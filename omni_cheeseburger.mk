a0c681129caebae286c149e7c85d7d57ac3863a0
Martinusbe <martinusbe@gmail.com>
2017-07-16 01:22:05 +0200

    lets start by converting
     * clean up coming
    
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- omni_cheeseburger.mk
+++ omni_cheeseburger.mk
@@ -14,23 +14,27 @@
 # limitations under the License.
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := cheeseburger
+
+$(call inherit-product, build/target/product/embedded.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from cheeseburger device
-$(call inherit-product, device/oneplus/cheeseburger/device.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit some common Tipsy stuff.
-$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
+# Time Zone data for recovery
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
-PRODUCT_NAME := tipsy_cheeseburger
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := cheeseburger
-PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_NAME := omni_cheeseburger
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := ONEPLUS A5000
-
-PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+PRODUCT_MODEL := OnePlus A5000
+PRODUCT_MANUFACTURER := OnePlus
 
 TARGET_VENDOR_PRODUCT_NAME := OnePlus5
 TARGET_VENDOR_DEVICE_NAME := OnePlus5
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo omni_cheeseburger-eng
+add_lunch_combo omni_cheeseburger-userdebug
+
